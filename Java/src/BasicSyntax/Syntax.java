b0aa1942302928aa0050abe09c337b6af03a5d65
k4ik <kaikleal1234@gmail.com>
2023-01-23 19:55:57 -0300

    rename file

--- Java/src/BasicSyntax/Syntax.java
+++ Java/src/BasicSyntax/Syntax.java
@@ -1,7 +1,7 @@
 package BasicSyntax;
 
-public class App {
+public class Syntax {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
     }
 }

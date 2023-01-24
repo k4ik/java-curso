9e4f012f7cc11d96a5e1b4ef5d55f3b75b8bb021
k4ik <kaikleal1234@gmail.com>
2023-01-24 15:00:58 -0300

    if

--- Java/src/If/If.java
+++ Java/src/If/If.java
@@ -1,5 +1,24 @@
 package If;
 
 public class If {
-    
+    public static void main(String[] args) {
+        float primeiraNota = 8.2f;
+        float segundaNota = 7.4f;
+        float terceiraNota = 9.1f;
+        float media = (primeiraNota + segundaNota + terceiraNota) / 3;
+
+        if (media <= 5) {
+            System.out.println("Reprovado");
+        } else if (media == 6) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Aprovado");
+        }
+        System.out.println(media);
+
+
+        int hora = 5;
+        String resultado = (hora > 12) ? "Boa tarde" : "Bom dia";
+        System.out.println(resultado);
+    }
 }

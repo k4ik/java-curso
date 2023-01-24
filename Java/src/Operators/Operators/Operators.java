e605810e6dc83e75c897b7ecc75c8fd4a8c1df43
k4ik <kaikleal1234@gmail.com>
2023-01-23 23:51:09 -0300

    operators

---
+++ Java/src/Operators/Operators/Operators.java
@@ -0,0 +1,57 @@
+package Operators;
+public class Operators {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+        /*
+          + Addition
+          - Subtraction
+          * Multiplication
+          / Division
+          % Modulus
+          ++ Increment
+          -- Decrement
+         */
+
+        int sum1 = 100 + 50;        // 150 (100 + 50)
+        int sum2 = sum1 + 250;      // 400 (150 + 250)
+        int sum3 = sum2 + sum2;     // 800 (400 + 400)
+
+        // Assignment Operators
+        /*
+            =	
+            +=	
+            -=		
+            *=	
+            /=	
+            %=	
+            &=	
+            |=	
+            ^=	
+            >>=	
+            <<=
+        */
+        int x = 10;
+        x += 5;
+
+        // Comparison Operators
+        /*
+            ==
+            !=
+            >
+            <
+            >=
+            <=
+        */
+        int y = 5;
+        int z = 3;
+        System.out.println(y > z); 
+
+
+        // Logical Operators
+        /*
+         && And
+         || Or
+         !  Not
+         */
+    }
+}

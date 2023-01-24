83d2df33a9ce3003d42717750328014d1d5fa873
k4ik <kaikleal1234@gmail.com>
2023-01-24 16:39:26 -0300

    create method

---
+++ Java/src/Methods/Methods.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Methods {
+    static void helloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        helloWorld();
+    }
+}

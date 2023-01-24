23908083eb3077a13287506a622a94ffff49fd53
k4ik <kaikleal1234@gmail.com>
2023-01-23 22:25:35 -0300

    variables

---
+++ Java/src/Variables/Variables.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Variables Types
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        // Final Variables
+        final int num = 15;
+        //num = 20; 
+    }
+}

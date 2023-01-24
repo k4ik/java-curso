f29fc2cb3b98a1bd00ea7a6f88817877d4d3da22
k4ik <kaikleal1234@gmail.com>
2023-01-24 17:17:06 -0300

    methods

--- Java/src/Methods/Methods.java
+++ Java/src/Methods/Methods.java
@@ -5,7 +5,23 @@ public class Methods {
         System.out.println("Hello World!");
     }
 
-    public static void main(String[] args) {
-        helloWorld();
-    }
+    static void checkAge(int age) {
+        if ( age < 18 ) {
+            System.out.println("Menor de Idade");
+        } else {
+            System.out.println("Maior de Idade");
+        }
+    } 
+
+    static int soma(int x, int y) {
+        return x + y;
+      }
+    
+      public static void main(String[] args) {
+        int z = soma(5, 3);
+        System.out.println(z);
+
+
+        checkAge(56);
+      }
 }

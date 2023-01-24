081a553e6a91d4f707bed93ed9b1bd6a1d85d4fa
k4ik <kaikleal1234@gmail.com>
2023-01-24 09:09:44 -0300

    special characters

---
+++ Java/src/Strings/SpecialCharacters.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class SpecialCharacters {
+    public static void main(String[] args) {
+        /*
+            \'	'	Single quote
+            \"	"	Double quote
+            \\	\	Backslash
+         */
+        String txt = "We are the so-called \"Vikings\" from the north.";
+        System.out.println(txt);
+
+        /*
+            \n	New Line	
+            \r	Carriage Return	
+            \t	Tab	
+            \b	Backspace	
+            \f	Form Feed
+         */
+
+
+    }
+}

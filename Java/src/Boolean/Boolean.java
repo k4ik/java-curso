92cdc8f157a3439ed065131ea182bce62f27129f
k4ik <kaikleal1234@gmail.com>
2023-01-24 14:39:51 -0300

    bool

---
+++ Java/src/Boolean/Boolean.java
@@ -0,0 +1,18 @@
+package Boolean;
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean carteiraDeTrabalho = false;
+        boolean maiorDeIdade = true;
+        System.out.println(carteiraDeTrabalho);
+        System.out.println(maiorDeIdade);
+
+        int x = 5;
+        int y = 7;
+        int z = x + y;
+        System.out.println(x > y);
+        System.out.println(z < y);
+        System.out.println(y != x);
+        System.out.println(z == (x + y));
+    }
+}
---
+++ Java/src/If/If.java
@@ -0,0 +1,5 @@
+package If;
+
+public class If {
+    
+}

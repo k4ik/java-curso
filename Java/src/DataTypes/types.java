dd39a2a82bcd66bae9684037730c056bff4dce37
k4ik <kaikleal1234@gmail.com>
2023-01-23 22:35:53 -0300

    types

---
+++ Java/src/DataTypes/types.java
@@ -0,0 +1,47 @@
+package DataTypes;
+
+public class types {
+    public static void main(String[] args) {
+        // Integer Types
+
+        /*
+         * Byte
+         * whole numbers from -128 to 127
+         */
+        byte num = 100;
+        System.out.println(num);
+
+        /*
+         * Short
+         * whole numbers from -32768 to 32767
+         */
+        short num2 = 5000;
+        System.out.println(num2);
+
+        /*
+         * Int
+         * numbers from -2147483648 to 2147483647
+         */
+        int num3 = 100000;
+        System.out.println(num3);
+
+        /*
+         * Long
+         * numbers from -9223372036854775808 to 9223372036854775807
+         * must end the value with an "L"
+         */
+        long num4 = 15000000000L;
+        System.out.println(num4);
+
+        // Floating Point Types
+
+        // Float
+        float num5 = 5.75f;
+        System.out.println(num5);
+
+        // Double
+        double num6 = 19.99d;
+        System.out.println(num6);
+
+    }
+}
